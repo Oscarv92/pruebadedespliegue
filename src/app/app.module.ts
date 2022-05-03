e175294fbe415a2511a3e463ad3fe44b98bcaa59
Oscarv92 <oscvasquezl92@gmail.com>
2022-05-02 19:32:12 -0500

    prueba de despliegue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarraComponent } from './componente/barra/barra.component';
+import { Formulario1Component } from './componente/formulario1/formulario1.component';
+import { CarruselComponent } from './componente/carrusel/carrusel.component';
+import { CardsComponent } from './componente/cards/cards.component';
+import { FooterComponent } from './componente/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraComponent,
+    Formulario1Component,
+    CarruselComponent,
+    CardsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
